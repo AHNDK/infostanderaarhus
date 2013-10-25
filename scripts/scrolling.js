acacad043e258d222913c1c6ef3afd9349f66378
Brian Jensen <mail@magnify.dk>
2013-10-25 14:43:38 +0200

    Added extra class check

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -19,13 +19,13 @@ $(document).ready(function() {
 
   // Show or hide top link when scrolling
   $(window).scroll(function() {
-    if ($(document).scrollTop() > 250) {
+    if (!$('body').hasClass('js-scrolling') && $(document).scrollTop() > 250) {
       // Add class to body
       $('body').addClass('js-scrolling');
 
       // Show arrow link
       $(topLink).show();
-    } else {
+    } else if (!$('body').hasClass('js-scrolling')) {
       // Remove class to body
       $('body').removeClass('js-scrolling');
 
