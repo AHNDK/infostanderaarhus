ace0c73119bed5ce0571f47d2a6a30db49a3893d
Brian Jensen <mail@magnify.dk>
2013-10-28 10:12:14 +0100

    Removed script and used jquery instead

--- contact_form.php
+++ contact_form.php
@@ -2,37 +2,36 @@
 
 $name = strip_tags($_POST["name"]);
 $email = strip_tags($_POST["email"]);
-$spam_check = $_POST["message"];
 
-$recipient = "infostander@aarhus.dk"; //Hvem skal have mailen?
+$recipient = "mail@magnify.dk";
 
-if ( (!empty($name) && !empty($email)) && empty($spam_check) )  {
+if (!empty($name) && !empty($email)) {
   $subject = "Jeg vil gerne kontaktes vedr. Infostander Aarhus"; //Emnefeltet til emailen.
 
-  $message = "<h1>" . $name . " vil gerne kontaktes</h1>";
-  $message.= "<p>Der er kommet en henvendelse fra " . $name . " på infostanderaarhus.dk, du bedes tage kontakte:</p>";
+  $message = "<h1>Jeg vil gerne kontaktes</h1>";
+  $message.= "<p>Der er kommet en henvendelse på infostanderaarhus.dk, du bedes kontakte:</p>";
   $message.= "<ul>";
   $message.= "<li><strong>Navn:</strong> ". $name ."</li>";
   $message.= "<li><strong>Email:</strong> ". $email ."</li>";
   $message.= "</ul>";
-  $message.= "<p>Med venlig hilsen</p>";
-  $message.= "<p>Infostander hjemmesiden.</p>";
 
   $header  = "MIME-Version: 1.0" . "\r\n";
   $header .= "Content-type: text/html; charset=utf-8" . "\r\n";
-  $header .= "From: Infostander kontaktformular <www@aakb.dk>" . "\r\n";
-  $header .= "Reply-To: " . $email;
+  $header .= "from: Infostander kontaktformular <www@aakb.dk>";
+  $header .= "reply-to:".$email;
 
   if (mail($recipient, $subject, $message, $header)) {
-    print '<div class="message--success"><h1>Vi takker for din henvendelse</h1>';
-    print "<p>Du h&oslash;rer fra os hurtigst muligt.</p></div>";
+    print "Tak for din henvendelse, du h&oslash;rer fra os snarest.";
   } else {
-    print '<div class="message--error"><h1>&Oslash;v! vores server meldte fejl.</h1>';
-    print "<p>Det lykkedes ikke for vores server at sende os en notifikation om din henvendelse.</p>";
-    print "<p>Det er vi rigtig kede af,</p>";
-    print "<p>kontakt os venligst direkte p&aring; email: ". $recipient . " s&aring; kan vi b&aring;de kigge p&aring; fejlen og hj&aelig;lpe dig.</p></div>";
+    print "Der skete en fejl og det lykkedes desværre ikke at sende din henvendelse.";
+    print "<br /><br />";
+    print "<a href=\"#kontakt\">Klik her for at prøve igen</a>. husk at alle felter skal udfyldes.";
+    print "<br /><br />";
+    print "Hvis du stadig oplever fejl, så kontakt os på p&aring; ". $recipient . ".";
+
+    http_response_code(406);
   }
 
 } else {
-  print '<div class="message--error">Vi kunne ikke behandle din henvendelse da formularen ikke blev udfyldt.</div>';
+  print "Du skal udfylde alle felter i formularen, prøv venligst igen.";
 }
