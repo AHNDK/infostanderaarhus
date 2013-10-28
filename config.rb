5e62fb07ec3c1e919d6f329b8d1e6e54bbf35abf
Brian Jensen <mail@magnify.dk>
2013-10-28 10:05:51 +0100

    Changed settings to production

--- config.rb
+++ config.rb
@@ -8,14 +8,14 @@ images_dir = "images"
 javascripts_dir = "scripts"
 additional_import_paths = ["/Library/Ruby/Gems/1.8/gems/zen-grids-1.4/stylesheets"]
   
-output_style = :expanded
-environment = :development
+output_style = :compressed
+environment = :production
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 color_output = false
 
 
